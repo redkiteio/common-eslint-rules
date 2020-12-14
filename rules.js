7f1fb66d5a3c27e062b06fa3002bb2f64ae571c7
Makar Melikyan <68788448+tinkermakar@users.noreply.github.com>
2020-12-14 00:10:47 +0000

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,21 +1,21 @@
-// v.1.0 (2020-12-14)
 const rules = {
-    'arrow-parens': [2, 'as-needed'],
-    'brace-style': [2, 'stroustrup'],
-    'class-methods-use-this': 0,
-    'func-names': [2, 'always', { generators: 'never' }],
-    'import/prefer-default-export': 0,
-    'react/jsx-one-expression-per-line': [2, { allow: 'single-child' }],
-    'linebreak-style': [2, 'unix'],
-    'object-curly-newline': [
-      0,
-      {
-        ObjectExpression: 'always',
-        ObjectPattern: { minProperties: 2 },
-      },
-    ],
-    'react/jsx-props-no-spreading': 0,
-    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
-  };
+  // v.1.0 (2020-12-14)
+  'arrow-parens': [2, 'as-needed'],
+  'brace-style': [2, 'stroustrup'],
+  'class-methods-use-this': 0,
+  'func-names': [2, 'always', { generators: 'never' }],
+  'import/prefer-default-export': 0,
+  'react/jsx-one-expression-per-line': [2, { allow: 'single-child' }],
+  'linebreak-style': [2, 'unix'],
+  'object-curly-newline': [
+    0,
+    {
+      ObjectExpression: 'always',
+      ObjectPattern: { minProperties: 2 },
+    },
+  ],
+  'react/jsx-props-no-spreading': 0,
+  'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+};
 
 export default rules;
