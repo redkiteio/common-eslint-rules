509335103a280da0c9005926bc772120466e3c61
Makar Melikyan <68788448+tinkermakar@users.noreply.github.com>
2021-03-12 18:52:46 +0000

    With new React version importing React is no longer a must

--- rules.js
+++ rules.js
@@ -1,11 +1,12 @@
 const rules = {
-  // v.1.2 (2021-01-16)
+  // v.1.3 (2021-03-12)
   'arrow-parens': [1, 'as-needed'],
   'brace-style': [1, 'stroustrup'],
   'class-methods-use-this': 0,
   'func-names': [1, 'always', { generators: 'never' }],
   'import/prefer-default-export': 0,
   'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
+  'react/react-in-jsx-scope': 0,
   'linebreak-style': 0,
   'object-curly-newline': [
     0,
