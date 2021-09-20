98439c9a91c93b440427bb89bf4ec7e386635350
Makar Melikyan <68788448+tinkermakar@users.noreply.github.com>
2021-09-20 00:27:36 +0000

    added TS rules

--- rules.js
+++ rules.js
@@ -1,22 +1,25 @@
 const rules = {
-  // v.1.3 (2021-03-12)
-  'arrow-parens': [1, 'as-needed'],
-  'brace-style': [1, 'stroustrup'],
-  'class-methods-use-this': 0,
-  'func-names': [1, 'always', { generators: 'never' }],
-  'import/prefer-default-export': 0,
-  'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
-  'react/react-in-jsx-scope': 0,
-  'linebreak-style': 0,
-  'object-curly-newline': [
-    0,
-    {
-      ObjectExpression: 'always',
-      ObjectPattern: { minProperties: 2 },
-    },
-  ],
-  'react/jsx-props-no-spreading': 0,
-  'no-plusplus': [1, { allowForLoopAfterthoughts: true }],
-};
+    // v.1.4 (2021-09-20)
+    '@typescript-eslint/no-explicit-any': 0,
+    'arrow-parens': [1, 'as-needed'],
+    'brace-style': [1, 'stroustrup'],
+    'class-methods-use-this': 0,
+    'func-names': [1, 'always', { generators: 'never' }],
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'no-console': [1, { allow: ['error', 'info', 'warning'] }],
+    'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
+    'react/react-in-jsx-scope': 0,
+    'linebreak-style': 0,
+    'object-curly-newline': [
+      0,
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: { minProperties: 2 },
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'no-plusplus': [1, { allowForLoopAfterthoughts: true }],
+  };
 
 export default rules;
