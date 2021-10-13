f8482fee33a03046f8f6903dfc7280907aed471a
Makar Melikyan <68788448+tinkermakar@users.noreply.github.com>
2021-10-13 23:29:17 +0000

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,5 +1,5 @@
 const rules = {
-    // v.1.4 (2021-09-20)
+    // v.1.5 (2021-10-14)
     '@typescript-eslint/no-explicit-any': 0,
     'arrow-parens': [1, 'as-needed'],
     'brace-style': [1, 'stroustrup', { allowSingleLine: true }],
@@ -8,8 +8,7 @@ const rules = {
     'import/extensions': 0,
     'import/prefer-default-export': 0,
     'no-console': [1, { allow: ['error', 'info', 'warning'] }],
-    'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
-    'react/react-in-jsx-scope': 0,
+    'no-return-assign': 0,
     'linebreak-style': 0,
     'object-curly-newline': [
       0,
@@ -18,8 +17,12 @@ const rules = {
         ObjectPattern: { minProperties: 2 },
       },
     ],
-    'react/jsx-props-no-spreading': 0,
     'no-plusplus': [1, { allowForLoopAfterthoughts: true }],
+
+    // React specific
+    'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
   };
 
 export default rules;
