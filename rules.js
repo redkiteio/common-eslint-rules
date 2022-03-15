d2d94dad1be655bf45e0821e67bce553d4205aca
Makar <68788448+makar2@users.noreply.github.com>
2022-03-15 17:13:26 +0400

    rules adjusted to e2e tests

--- rules.js
+++ rules.js
@@ -1,5 +1,5 @@
 const rules = {
-    // v.1.5 (2021-10-14)
+    // v.1.6 (2022-03-14)
     '@typescript-eslint/no-explicit-any': 0,
     'arrow-parens': [1, 'as-needed'],
     'brace-style': [1, 'stroustrup', { allowSingleLine: true }],
@@ -8,6 +8,8 @@ const rules = {
     'import/extensions': 0,
     'import/prefer-default-export': 0,
     'no-console': [1, { allow: ['error', 'info', 'warning'] }],
+    'no-param-reassign': [1, { props: false }],
+    'no-plusplus': [1, { allowForLoopAfterthoughts: true }],
     'no-return-assign': 0,
     'linebreak-style': 0,
     'object-curly-newline': [
@@ -17,7 +19,7 @@ const rules = {
         ObjectPattern: { minProperties: 2 },
       },
     ],
-    'no-plusplus': [1, { allowForLoopAfterthoughts: true }],
+    radix: [1, 'as-needed'],
 
     // React specific
     'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
